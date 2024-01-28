9594ab5648b26fb42fc2b6d25dc34790ff1fa672
KrishChettri <140698645+KrishChettri@users.noreply.github.com>
2024-01-28 15:52:05 +1300

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBKqhOYgBwnuTUNBSF0uZMLA2Lf6ljyQEc",
+    authDomain: "lets-chat-web-app-eff89.firebaseapp.com",
+    databaseURL: "https://lets-chat-web-app-eff89-default-rtdb.firebaseio.com",
+    projectId: "lets-chat-web-app-eff89",
+    storageBucket: "lets-chat-web-app-eff89.appspot.com",
+    messagingSenderId: "270594270833",
+    appId: "1:270594270833:web:51b096699507fdda4f5dd0"
+  };
